26ee0deaac6747ce13650b8a0ac6072a96a16d33
segy2291 <sagar2209.ritz@gmail.com>
2016-12-15 16:42:56 -0500

    add patient data

--- ArrowHCiOS/AddPatientViewController.swift
+++ ArrowHCiOS/AddPatientViewController.swift
@@ -14,13 +14,26 @@ class AddPatientViewController: UIViewController, UIPickerViewDelegate,UIPickerV
     @IBOutlet weak var pname: UITextField!
     
     
-    @IBOutlet weak var department: UILabel!
+    
+   // @IBOutlet weak var department: UILabel!
    
-    @IBOutlet weak var roomnumber: UILabel!
+    //@IBOutlet weak var roomnumber: UILabel!
 
     @IBOutlet weak var saveButton: UIBarButtonItem!
     
+    @IBOutlet weak var department: UILabel!
+    
+    
+    @IBOutlet weak var roomnumber: UILabel!
+    
     @IBOutlet weak var nursename: UILabel!
+    
+    
+    
+    
+    
+    
+   // @IBOutlet weak var nursename: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -97,28 +110,40 @@ class AddPatientViewController: UIViewController, UIPickerViewDelegate,UIPickerV
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
-        
+    
+    let patientTableViewController = segue.destinationViewController as! PatientTableViewController
+    
+    let personID = patientTableViewController.doctorId
+    
         let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
         
         let request = NSMutableURLRequest(URL: NSURL(string: "https://arrowhc.herokuapp.com/patient")!)
         request.HTTPMethod = "POST";
-        
-        let jsonDict = ["patient_name": "Maria5",
-                        "room_no": "101",
-                        "username": "maria5",
+      //  String(self.pname.text)
+    
+    
+           let name=pname.text
+    let dept=department.text
+    let room=roomnumber.text
+    let nurse=nursename.text
+    
+    //let dortorid = PatientTableViewController.doc
+        let jsonDict = ["patient_name":name!,
+                        "room_no":room!,
+                        "username": String(NSDate()),
                         "password": "bonita",
-                        "doc_id": "583dcc7e5a034e0519757632",
+                        "doc_id": personID,
                         "doc_name": "Dr Luis",
                         "doc_username": "luis",
-                        "department": "Neurology",
+                        "department": dept!,
                         "nurse_id": "583dccc05a034e0519757633",
-                        "nurse_name": "Nr Laura",
+                        "nurse_name": nurse!,
                         "nurse_username": "eli",
                         "profile": "patient",
-                        "name": "Maria5",
+                        "name":name!,
                         "email": "testprofile@hotmail.com",
                         "phone": "6478887755"] as Dictionary<String, String>
         
@@ -137,7 +162,7 @@ class AddPatientViewController: UIViewController, UIPickerViewDelegate,UIPickerV
             }
         }
         task.resume()
-        
+ 
         
     }
 
